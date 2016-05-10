d9d43273ba24d508141eb732ce40a2df32fa6ae3
Meuli Lukas <ml@gilgen.com>
2016-05-10 16:02:19 +0200

    Original

---
+++ DelegateUhr/KukusUhr.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace DelegateUhr
+{
+    public sealed class KukusUhr
+    {
+        // Die Methode soll die Zeit ausgeben
+        public delegate void ClockDelegat(DateTime time);
+
+        //privat von aussen geschützt
+        private ClockDelegat DelegatedMethod;
+
+
+        public event ClockDelegat Tick;
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        public KukusUhr()
+        {
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += timer_Elapsed;
+            timer.Start();
+        }
+
+        //public void Register(ClockDelegat delegatedMethod)
+        //{
+        //    DelegatedMethod += delegatedMethod;
+        //}
+
+        //public void Unregister(ClockDelegat callback)
+        //{
+        //    DelegatedMethod -= callback;
+        //}
+
+        void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            //Ab VS2015 wäre folgndes möglich
+            //DelegatedMethod?.Invoke(e.SignalTime);
+
+            //if (DelegatedMethod != null)
+            //{
+            //    DelegatedMethod(e.SignalTime);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(".");
+            //}
+
+            //Tick.Invoke(e.SignalTime);
+            if (Tick != null)
+            {
+                Tick.Invoke(e.SignalTime);
+            }
+            else
+            {
+                Console.WriteLine(".");
+            }
+
+
+            //foreach (ClockDelegat callback in DelegatedMethod.GetInvocationList())
+            //{
+            //    try
+            //    {
+            //    callback(e.SignalTime);
+            //    }
+                //catch (Exception exception)
+                //{
+                //    Console.WriteLine(exception.Message);
+                //}
+            //}
+        }
+
+
+    }
+}
---
+++ DelegateUhr/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DelegateUhr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KukusUhr mykukUhr = new KukusUhr();
+
+
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+
+            //mykukUhr.Register(ShowTime);
+            mykukUhr.Tick += ShowTime;
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+
+            //mykukUhr.Register(ShowDate);
+            mykukUhr.Tick += ShowDate;
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+
+            //mykukUhr.Unregister(ShowDate);
+            mykukUhr.Tick -= ShowTime;
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+
+            //mykukUhr.Unregister(ShowTime);
+            mykukUhr.Tick -= ShowDate;
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+
+        }
+
+
+        public static void ShowTime(DateTime time)
+        {
+            Console.WriteLine(time.ToLongTimeString());
+        }
+           public static void ShowDate(DateTime time)
+        {
+            Console.WriteLine(time.ToLongDateString());
+
+            // zu testzwecken
+           // throw new Exception("Something went wrong");
+
+        }
+    } 
+}
